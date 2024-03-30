b601cbee63b92cca196f8a4ad529925865e971a9
Linus Bolls <linusbolls@Linuss-Air.fritz.box>
2024-03-30 01:42:13 +0100

    feat: hook semesters kanban up to learning platform api

--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -1,4 +1,14 @@
-import { Button, Flex, Input, Modal, Typography } from "antd";
+import {
+  Button,
+  Collapse,
+  Flex,
+  Image,
+  Input,
+  Modal,
+  Steps,
+  Typography,
+} from "antd";
+import Link from "next/link";
 import { useState } from "react";
 
 export interface LoginModalProps {
@@ -8,35 +18,104 @@ export default function LoginModal({ onSubmit }: LoginModalProps) {
   const [token, setToken] = useState("");
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-
-        onSubmit(token);
-      }}
+    <Modal
+      open
+      closable={false}
+      okButtonProps={{ style: { display: "none" } }}
+      cancelButtonProps={{ style: { display: "none" } }}
+      width="42rem"
     >
-      <Modal
-        open
-        closable={false}
-        okButtonProps={{ style: { display: "none" } }}
-        cancelButtonProps={{ style: { display: "none" } }}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          onSubmit(token);
+        }}
       >
-        <Typography.Title level={4}>Welcome to Study Planner</Typography.Title>
-        Please enter your access token for the CODE Learning Platform.
+        <Typography.Title level={2}>Welcome to Study Planner</Typography.Title>
+        Thank you for using Study Planner! We just need one thing from you:
         <Flex vertical gap="medium">
           <Input
             type="password"
-            placeholder="Access token"
+            placeholder="Your access token for the CODE Learning Platform"
             style={{ marginTop: "1rem" }}
             size="large"
             value={token}
             onChange={(e) => setToken(e.target.value)}
           />
           <Flex justify="end">
-            <Button type="primary">Submit</Button>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
           </Flex>
         </Flex>
-      </Modal>
-    </form>
+        {/* <Collapse title="Why do we need this?"></Collapse> */}
+        <Typography.Title level={4}>Why do we need this?</Typography.Title>
+        <Typography.Text>
+          We interact with the Learning Platform API to see what modules are
+          available to you and which ones you&apos;ve already taken. We
+          don&apos;t store any of this data; You are the only one that will see
+          it.
+        </Typography.Text>
+        <Typography.Title level={4}>How do I get this?</Typography.Title>
+        <Image
+          src="https://github.com/LinusBolls/code-university-sdk/raw/main/docs/getting-your-access-token.webp"
+          alt="Screenshot of the cookies tab"
+        />
+        <Steps
+          current={-1}
+          direction="vertical"
+          items={[
+            {
+              title: (
+                <Typography.Text>
+                  Open{" "}
+                  <Link href="https://app.code.berlin">
+                    https://app.code.berlin
+                  </Link>
+                </Typography.Text>
+              ),
+            },
+            {
+              title: `Open the browser devtools`,
+              description:
+                "Using Cmd + Shift + I on mac or Ctrl + Shift + I on windows",
+            },
+            {
+              title: "Click on the Application tab",
+            },
+            {
+              title: `Double-click the Value field of the cid cookie, and copy it to your clipboard.`,
+            },
+          ]}
+        />
+        {/* <ol>
+          <li>
+            <Typography.Text>Open https://app.code.berlin</Typography.Text>
+          </li>
+          <li>
+            <Typography.Text>
+              Open the browser devtools using Cmd + Shift + I on mac or Ctrl +
+              Shift + I on windows
+            </Typography.Text>
+          </li>
+          <li>
+            <Typography.Text>Click on the Application tab</Typography.Text>
+          </li>
+          <li>
+            <Typography.Text>
+              On the left side of the Application tab, click Cookies {">"}{" "}
+              https://app.code.berlin
+            </Typography.Text>
+          </li>
+          <li>
+            <Typography.Text>
+              Double-click the Value field of the cid cookie, and copy it to
+              your clipboard.
+            </Typography.Text>
+          </li>
+        </ol> */}
+      </form>
+    </Modal>
   );
 }
