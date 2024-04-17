c06efb7a73fddd27dd7fc4620f6ff7379e78104c
Linus Bolls <linusbolls@Linuss-MacBook-Air.local>
2024-04-17 17:40:19 +0200

    feat: opengraph link preview banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,27 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Metadata } from "next";
+import { env } from "@/backend/env";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Study Planner for CODE",
   description: "Comfortably plan out your remaining semesters at CODE",
+  openGraph: {
+    type: "website",
+    url: env.publicAppUrl,
+    title: "Study Planner for CODE",
+    description: "Comfortably plan out your remaining semesters at CODE",
+    images: [
+      {
+        url: env.publicAppUrl + "/opengraph-thumbnail.png",
+        width: 1200,
+        height: 630,
+        alt: "Study Planner for CODE",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
