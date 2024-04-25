b35433ef7113b966151b8b7fb0c5efa3571797ac
Linus Bolls <linusbolls@Linuss-MacBook-Air.local>
2024-04-25 18:57:33 +0200

    feat: improve guide on how to get your access token

--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -52,10 +52,10 @@ export default function LoginModal({ onSubmit }: LoginModalProps) {
         {/* <Collapse title="Why do we need this?"></Collapse> */}
         <Typography.Title level={4}>Why do we need this?</Typography.Title>
         <Typography.Text>
-          We interact with the Learning Platform API to see what modules are
-          available to you and which ones you&apos;ve already taken. We
-          don&apos;t store any of this data; You are the only one that will see
-          it.
+          The Study Planner interacts with the Learning Platform API to see what
+          modules are available to you and which ones you&apos;ve already taken.
+          We don&apos;t store any of this data, you are the only one that will
+          see it. We also don&apos;t store your access token.
         </Typography.Text>
         <Typography.Title level={4}>How do I get this?</Typography.Title>
         <Image
@@ -70,51 +70,48 @@ export default function LoginModal({ onSubmit }: LoginModalProps) {
               title: (
                 <Typography.Text>
                   Open{" "}
-                  <Link href="https://app.code.berlin">
+                  <Link href="https://app.code.berlin" target="_blank">
                     https://app.code.berlin
                   </Link>
                 </Typography.Text>
               ),
             },
             {
-              title: `Open the browser devtools`,
-              description:
-                "Using Cmd + Shift + I on mac or Ctrl + Shift + I on windows",
+              title: (
+                <Typography.Text>Open the browser devtools</Typography.Text>
+              ),
+              description: (
+                <>
+                  Using Cmd + Shift + I on mac or Ctrl + Shift + I on windows.
+                </>
+              ),
             },
             {
-              title: "Click on the Application tab",
+              title: <Typography.Text>View your cookies</Typography.Text>,
+              description: (
+                <>
+                  By going to the <i>Application tab</i>, expanding the{" "}
+                  <i>Cookies</i> item on the left, and selecting{" "}
+                  <i>https://app.code.berlin</i>. If the <i>cid</i> cookie
+                  doesn&apos;t show up, try refreshing the browser tab.
+                </>
+              ),
             },
             {
-              title: `Double-click the Value field of the cid cookie, and copy it to your clipboard.\nIf the cid cookie doesn't show up, try refreshing the browser tab.`,
+              title: (
+                <Typography.Text>
+                  Copy the value of the <i>cid</i> cookie
+                </Typography.Text>
+              ),
+              description: (
+                <>
+                  By double-clicking the field, and copying it to your
+                  clipboard.
+                </>
+              ),
             },
           ]}
         />
-        {/* <ol>
-          <li>
-            <Typography.Text>Open https://app.code.berlin</Typography.Text>
-          </li>
-          <li>
-            <Typography.Text>
-              Open the browser devtools using Cmd + Shift + I on mac or Ctrl +
-              Shift + I on windows
-            </Typography.Text>
-          </li>
-          <li>
-            <Typography.Text>Click on the Application tab</Typography.Text>
-          </li>
-          <li>
-            <Typography.Text>
-              On the left side of the Application tab, click Cookies {">"}{" "}
-              https://app.code.berlin
-            </Typography.Text>
-          </li>
-          <li>
-            <Typography.Text>
-              Double-click the Value field of the cid cookie, and copy it to
-              your clipboard.
-            </Typography.Text>
-          </li>
-        </ol> */}
       </form>
     </Modal>
   );
